9375e0fdb783452066e878d5fe79315248de242d
“Finn <fwhite@chapman.edu>
2019-11-08 10:34:30 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# username.sh
+# Finn White
+echo "Enter a username: "
+echo "(Username must be 3-12 characters and only use lower case letters, digits, and '_' AND must start with a lower case letter)"
+read username
+while echo "$username" | egrep -v "^[a-z][0-9 a-z _]{2,11}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username - (a-z), (0-9), (_), 3-12 characters, lowercase first character!"
+	echo "Enter a valid username: "
+	read username
+done
+echo "Thank you"
